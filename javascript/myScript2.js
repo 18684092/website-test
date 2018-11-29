7a919b3aa3314508d2c7594c02d7e974fbd3b524
Andy P <18684092@students.lincoln.ac.uk>
2018-11-29 12:31:05 +0000

    6

--- javascript/myScript2.js
+++ javascript/myScript2.js
@@ -13,7 +13,7 @@ function uniCharCode(event)
     var key = event.keyCode;
 	console.log(key);
 	if (key == 65)
-		localStorage.setItem("test", "£");
+		localStorage.setItem("test", "€");
 	else
 		localStorage.setItem("test", "rubbish");
 	document.getElementById("p2").innerHTML = localStorage.getItem("test");
